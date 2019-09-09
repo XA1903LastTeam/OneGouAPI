e1111364314b9096da6356400fad70d209675acb
imzhangao <imzhangao@163.com>
2019-09-09 15:26:21 +0800

    'uuid数据BUG'

--- common/__init__.py
+++ common/__init__.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 
 
 class YGBaseModel(models.Model):
-    id = models.UUIDField(primary_key=True, verbose_name='ID')
+    id = models.CharField(max_length=50, primary_key=True, verbose_name='ID')
 
     class Meta:
         abstract = True
