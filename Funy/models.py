15093bebd9539e3f950da4fbbedcc93392137c57
zheng2ke <1420727661@qq.com>
2019-09-09 17:51:20 +0800

    '订单'

--- Funy/models.py
+++ Funy/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from common import YGBaseModel
+class cartModel(YGBaseModel):
+
+    user_id = models.OneToOneField('',on_delete=models.CASCADE,verbose_name='用户ID')
+    def __str__(self):
+        return self.user_id
+
+
+
+class orderModel(YGBaseModel):
+
+    user_id = models.IntegerField(max_length=50,verbose_name='用户ID')
+
+
+class order_listModel(YGBaseModel):
+    order_id = models.OneToOneField('',on_delete=models.CASCADE,verbose_name='订单ID')
+    start_time = models.DateTimeField(max_length=50,verbose_name='下单时间')
+    order_statud = models.IntegerField(choices=((0,'待支付'),(1,'已支付'),(2,'已取消'),(3,'待发货'),(4,'已发货'),(5,'已完成')),verbose_name='订单状态')
+    goods_id = models.ManyToManyField('',related_name='goods',db_table='t_goods_order_list',verbose_name='商品ID')
+    count = models.IntegerField(max_length=50,verbose_name='商品数量')
+
+    card_id = models.IntegerField(max_length=50,verbose_name='所属购物车')
+
+    def __str__(self):
+        return self.order_id
+
+    @property
+    def count_price(self):
+        return self.goods_id.originalprice * self.count
+
+
+
 
 # Create your models here.
