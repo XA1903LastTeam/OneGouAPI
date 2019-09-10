4f71c9615cdf8bd964577b7d2941d1f5045e3bad
wangjiechao123 <745802359@qq.com>
2019-09-10 21:21:35 +0800

    '随'

--- UserApp/views.py
+++ UserApp/views.py
@@ -13,15 +13,9 @@ class UserAPIView(View):
         if not login:
             datas = UserModel.objects.all()
             serializer = UserSeraLizer(datas, many=True)
-<<<<<<< HEAD
 
-            data = JSONRenderer().render(serializer.data)
-            data = JSONParser().parse(io.BytesIO(data))
 
-        return JsonResponse({'data': data})
+
 
             return JsonResponse({ 'data':serializer.data })
 
-=======
-            return JsonResponse({ 'data':serializer.data })
->>>>>>> 57b859ac59c4c116f344f6f190f4ebd3e9b0b437
