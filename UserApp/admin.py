056b34d7f5e2c328905a4e1996c142329ba69c47
xiaoqiangnobug <15229360298@163.com>
2019-09-10 14:13:14 +0800

    '定制用户模型字段'

--- UserApp/admin.py
+++ UserApp/admin.py
@@ -6,16 +6,17 @@ from UserApp.models import UserModel, CommentsModel, NavModel
 
 class UserModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'phone', 'image', 'sex', 'bool')
+    fields = ('name', 'phone', 'image', 'sex', 'bool')
 
 
 class CommentsAdmin(admin.ModelAdmin):
     list_display = ('order_id', 'comments', 'comment_time')
-
+    fields = ('order_id', 'comments', 'comment_time')
 
 
 class NavModelAdmin(admin.ModelAdmin):
     list_display = ('nav_child_id', 'name', 'image')
-
+    fields = ('nav_child_id', 'name', 'image')
 
 admin.site.register(UserModel, UserModelAdmin)
 admin.site.register(CommentsModel, CommentsAdmin)
