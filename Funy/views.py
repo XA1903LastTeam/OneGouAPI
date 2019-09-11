c21cb6d76d2eb74182d77803ed8a9972e7293130
eatfishwithouttail <870815471@qq.com>
2019-09-11 11:12:05 +0800

    'ygeatfunny'

--- Funy/views.py
+++ Funy/views.py
@@ -5,14 +5,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
-from Funy.models import CategoryModel
-from .api import CategorySerlaizer
+from Funy.models import CategoryModel,YgeatModel
+from .api import CategoryModelSerializers,YgeatModelSerializers
 
 class CategoryView(View):
     def get(self,request):
         datas = CategoryModel.objects.filter(father_id='00000000-0000-0000-0000-000000000000').all()
-        ser = CategorySerlaizer(datas,many=True)
-
+        ser = CategoryModelSerializers(datas,many=True)
         return JsonResponse({
             'data':ser.data
         })
@@ -23,8 +22,17 @@ class CatechildView(View):
         if f_id:
             datas = CategoryModel.objects.filter(father_id=f_id).all()
         else:
-            datas = CategoryModel.objects.all()
-        ser = CategorySerlaizer(datas, many=True)
+            datas = CategoryModel.objects.filter(father_id='ccd5a8ef-66ca-48d2-962c-613d23500cf9')
+        ser = CategoryModelSerializers(datas, many=True)
         return JsonResponse({
             'data': ser.data
+        })
+
+class YgeatView(View):
+    def get(self,request):
+        datas = YgeatModel.objects.all()
+        ser = YgeatModelSerializers(datas,many=True)
+
+        return JsonResponse({
+            'data':ser.data
         })
