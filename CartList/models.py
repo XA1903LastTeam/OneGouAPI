025ba2a9bacc3291996bd772ffe8ff6322d45aa4
xiaoqiangnobug <15229360298@163.com>
2019-09-15 21:44:00 +0800

    '购物车订单模型修改'

--- CartList/models.py
+++ CartList/models.py
@@ -6,8 +6,15 @@ from common import YGBaseModel
 
 
 class CartModel(YGBaseModel):
-    user_id = models.OneToOneField('UserApp.UserModel', on_delete=models.CASCADE, verbose_name='用户ID',
-                                   related_name='user')
+    user = models.ForeignKey('UserApp.UserModel',
+                             on_delete=models.CASCADE,
+                             verbose_name='用户ID',
+                             related_name='user')
+    goods = models.ForeignKey('Goods.GoodsModel',
+                              on_delete=models.CASCADE,
+                              verbose_name='商品',
+                              related_name='cart')
+
 
     def __str__(self):
         return self.user_id
@@ -17,27 +24,13 @@ class CartModel(YGBaseModel):
         verbose_name_plural = verbose_name = '购物车表'
 
 
-class OrderModel(YGBaseModel):
-    user_id = models.ForeignKey('UserApp.UserModel', on_delete=models.CASCADE, verbose_name='用户订单ID',
-                                related_name='user_order')
-    card_id = models.ForeignKey('CartList.CartModel',
-                                verbose_name='所属购物车',
-                                related_name='orders',
-                                on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user_id.name
-
-    class Meta:
-        db_table = 't_order'
-        verbose_name_plural = verbose_name = '订单表'
 
 
 class Order_listModel(YGBaseModel):
-    order_id = models.OneToOneField('CartList.OrderModel',
-                                    on_delete=models.CASCADE,
-                                    verbose_name='订单ID',
-                                    related_name='order')
+    user = models.ForeignKey('UserApp.UserModel',
+                             on_delete=models.CASCADE,
+                             verbose_name='所属用户',
+                             related_name='order')
     start_time = models.CharField(max_length=50, verbose_name='下单时间')
     order_statud = models.IntegerField(choices=((0, '待支付'),
                                                 (1, '已支付'),
@@ -46,25 +39,30 @@ class Order_listModel(YGBaseModel):
                                                 (4, '已发货'),
                                                 (5, '已完成')),
                                        verbose_name='订单状态')
-    goods_id = models.ManyToManyField('Goods.GoodsModel', related_name='goods', db_table='t_goods_order_list',
-                                      verbose_name='商品ID')
-    count = models.IntegerField(verbose_name='商品数量')
-
     addr_id = models.ForeignKey('Address.AddressModel', on_delete=models.CASCADE, verbose_name='订单地址',
                                 related_name='addrs')
 
     def __str__(self):
-        return self.order_id_id
-
-
-    @property
-    def count_price(self):
-        return self.goods_id.goodshot* self.count
-
-    @property
-    def goods_name(self):
-        return self.goods_id.categoryid
+        return self.id
 
     class Meta:
         db_table = 't_orderlist'
         verbose_name_plural = verbose_name = '订单详情表'
+
+class OrderGoods(YGBaseModel):
+    order = models.ForeignKey('Order_listModel',
+                              on_delete=models.CASCADE,
+                              verbose_name='所属订单',
+                              related_name='goods')
+    goods = models.ForeignKey('Goods.GoodsModel',
+                              on_delete=models.CASCADE,
+                              verbose_name='商品',
+                              related_name='order')
+    count = models.IntegerField(verbose_name='购买数量')
+
+    def __str__(self):
+        return self.goods_id
+
+    class Meta:
+        db_table = 't_ordergoods'
+        verbose_name_plural = verbose_name = '订单商品详情表'
